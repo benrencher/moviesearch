dce722d7b8c086bf43f27f9f0c056528fc3e18e6
bdrencher <bdrencher@gmail.com>
2023-05-13 14:52:27 -0600

    changing title to 'Movie Search'

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'webapp',
+    title: 'Movie Search',
     htmlAttrs: {
       lang: 'en'
     },
