177cb9f3fb5755069006876fa71bc9b07a51aceb
bdrencher <bdrencher@gmail.com>
2023-05-13 14:51:15 -0600

    creating movie component

---
+++ webapp/components/Movie.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="movieRoot">
+    <h3>{{ title }}</h3>
+    <img :src="posterImageUrl" />
+    <p>{{ popularitySummary }}</p>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    title: String,
+    posterImageUrl: String,
+    popularitySummary: String
+  })
+</script>
+
+<style>
+  .movieRoot {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    row-gap: 8px;
+  }
+</style>
